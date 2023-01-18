664b4e55aa69ad04894284cdf900681f6edcad3e
Kaushik <50703773+thekaushikls@users.noreply.github.com>
2023-01-18 04:25:48 +0100

    [wip] test commit

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     print
     print(u"\u001b[32m PASS: \u001b[0m Loading Rhino...")
     result = subprocess.call(r'"C:\Program Files\Rhino 7\System\Rhino.exe" /nosplash /runscript="_-Grasshopper Banner Disable Window Load Window Show _Enter"')
-
+    
     print(result)
 
     print
